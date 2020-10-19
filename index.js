452bf97f239d00c4e09998586a96c3772f70a66d
Christine Moore <cmoorelvt@gmail.com>
2020-10-19 19:11:24 -0400

    input questions started

---
+++ index.js
@@ -0,0 +1,94 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "bio",
+        message: "Please give a description of your project."
+    },
+    {
+        type: "input",
+        name: "table",
+        message: "What is your favorite hobby?"
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "What is your favorite food?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Enter your GitHub Username"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Enter your LinkedIn URL."
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "Enter your LinkedIn URL."
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Enter your LinkedIn URL."
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Enter your LinkedIn URL."
+    },
+  ]);
+}
+
+function generateHTML(answers) {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+    <p class="lead">I am from ${answers.location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${answers.github}</li>
+      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+}
+
+promptUser()
+  .then(function(answers) {
+    const html = generateHTML(answers);
+
+    return writeFileAsync("index.html", html);
+  })
+  .then(function() {
+    console.log("Successfully wrote to index.html");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
